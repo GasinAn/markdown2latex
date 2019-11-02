18e3617039511d3d5d9b374b179c3f6588807bcf
fzyzcjy <turtlegood@163.com>
2019-11-02 16:28:35 +0800

    temp more

--- expr1-main/filter.py
+++ expr1-main/filter.py
@@ -1,17 +1,68 @@
-from pathlib import Path
-from panflute import *
+import io
+import re
+import subprocess
 
-TITLE_LINE = r"{\noindent{\bf %s}}"
+import panflute as pf
+
+
+def construct_doc_from_elems(elems, doc):
+    return pf.Doc(*elems, metadata=doc.metadata, format=doc.format, api_version=doc.api_version)
+
+
+def doc_to_json_str(doc):
+    stream = io.StringIO()
+    pf.dump(doc, stream)
+    return stream.getvalue()
+
+
+def json_str_to_latex(json_str):
+    # https://stackoverflow.com/questions/163542/python-how-do-i-pass-a-string-into-subprocess-popen-using-the-stdin-argument
+    cmd = ['pandoc', '-f', 'json', '-t', 'latex']
+    p = subprocess.run(cmd, stdout=subprocess.PIPE,
+                       input=json_str, encoding='utf-8')
+    assert p.returncode == 0
+    return p.stdout
+
+
+def postprocess_latex_newlines(latex_str: str):
+    in_lines = latex_str.split('\n')
+    out_lines = ['']
+    for i in range(len(in_lines)):
+        if in_lines[i] == '':
+            out_lines.append('')
+        out_lines[-1] += in_lines[i]
+    return '\n'.join(out_lines)
+
+
+def parse_simple_key_value_pairs(lines: str):
+    d = dict()
+    for line in lines.split('\n'):
+        m = re.match(r'^(\w+):(.+)$', line)
+        if m is not None:
+            d[m.group(1)] = m.group(2).lstrip()
+    return d
 
 
 def action(elem, doc):
-    if isinstance(elem, Image):
+    if isinstance(elem, pf.Image):
         blockquote = elem.parent.next
-        if isinstance(blockquote, BlockQuote):
+        if isinstance(blockquote, pf.BlockQuote):
             print(elem, blockquote)
+            elems_doc = construct_doc_from_elems(blockquote.content, doc)
+            print(elems_doc)
+            json_str = doc_to_json_str(elems_doc)
+            print(json_str)
+            latex_str = json_str_to_latex(json_str)
+            print(latex_str)
+            latex_str = postprocess_latex_newlines(latex_str)
+            print(latex_str)
+            kv_pair = parse_simple_key_value_pairs(latex_str)
+            print(kv_pair)
+
 
 def main(doc=None):
-    return run_filter(action, doc=doc) 
+    return pf.run_filter(action, doc=doc)
+
 
 if __name__ == '__main__':
     main()
--- expr1-main/markdown/content.md
+++ expr1-main/markdown/content.md
@@ -2,5 +2,9 @@
 
 ![](img.png)
 
-> key: value
-> k2 : val2
+> k1: v1
+>
+> k2: This is a very long sentence with @ and # and some other // symbols. We want to \cite{tom} and \LaTeX some things.
+>
+> 
+> k3: Hello World! $y=\mathcal{f}(x^x)$ is a math formula.
